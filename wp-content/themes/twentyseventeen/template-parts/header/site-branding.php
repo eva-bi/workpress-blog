fc6a48402b27046e03d66ebaee47ef13d2da6a39
eva-bi <eva.tuanla@gmail.com>
2017-10-12 17:01:50 +0700

    rm title

--- wp-content/themes/twentyseventeen/template-parts/header/site-branding.php
+++ wp-content/themes/twentyseventeen/template-parts/header/site-branding.php
@@ -15,11 +15,6 @@
 		<?php the_custom_logo(); ?>
 
 		<div class="site-branding-text">
-			<?php if ( is_front_page() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php endif; ?>
 
 			<?php
 			$description = get_bloginfo( 'description', 'display' );
